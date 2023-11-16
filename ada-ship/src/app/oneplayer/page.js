c5db43fa3b877d20b7a5b0dc6d7cc8fbeafb9040
Asher De Souza <77273625+asherddesouza@users.noreply.github.com>
2023-11-16 21:50:33 +0000

    removing boilerplate css

--- ada-ship/src/app/oneplayer/page.js
+++ ada-ship/src/app/oneplayer/page.js
@@ -1,7 +1,14 @@
+import styles from './page.css'
+
 export const metadata = {
     title: 'One Player',
   }
 
 export default function Play({searchParams}) {
-    return <h1>One Player, Gamemode: {searchParams.gamemode}</h1>
+    return <h1 className='cheese'>One Player, Gamemode: {searchParams.gamemode}</h1>
+
+    //fix css
+    //create a file parser to take what's in adaship_config to define ship and board sizes
+    //create a grid in HTML/CSS to mirror what is created within the code
+    //create a default array type then create two arrays from that, one target and one shipboard
 }
--- ada-ship/src/app/page.js
+++ ada-ship/src/app/page.js
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main>
+      <div>
         <h1
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert">
           AdaShip
@@ -13,23 +13,20 @@ export default function Home() {
       <p>A reimagined take on the classic game of Battleships.</p>
       <p>Choose from the options below to select your gamemode:</p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div>
         <Link
           href={{
             pathname: "/oneplayer",
             query: {gamemode: "normal"}
         }}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2>
             Player vs Computer{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span>
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p>
             Play against a computer-generated opponent.
           </p>
         </Link>
@@ -39,17 +36,14 @@ export default function Home() {
             pathname: "/twoplayer",
             query: {gamemode: "normal"}
         }}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2>
             Two Player{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span>
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p>
             Play locally against another person. [IN PROGRESS]
           </p>
         </Link>
@@ -58,18 +52,14 @@ export default function Home() {
           href={{
             pathname: "/oneplayer",
             query: {gamemode: "salvo"}
-        }}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+        }}>
+          <h2>
             Player vs Computer (Salvo){' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span>
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p>
             Play against a computer-generated opponent in Salvo mode. [IN PROGRESS]
           </p>
         </Link>
@@ -79,17 +69,14 @@ export default function Home() {
             pathname: "/twoplayer",
             query: {gamemode: "salvo"}
         }}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2>
             Two Player (Salvo){' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span>
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p>
             Play locally against another person in Salvo mode. [IN PROGRESS]
           </p>
         </Link>
